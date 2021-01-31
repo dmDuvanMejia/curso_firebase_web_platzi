7a98de7e3fcc918a9e531bb3513f61119494f67f
Duvan Mejia <dquiros@ospinternational.com>
2021-01-31 16:17:52 -0500

    Login con Facebook

--- public/js/auth/authController.js
+++ public/js/auth/authController.js
@@ -1,8 +1,8 @@
 $(() => {    
 
     const objAuth = new Autenticacion();
-    //$("#authFB").click(() => );
 
+    
     $("#btnRegistroEmail").click(() => {
         const nombres = $('#nombreContactoReg').val();
         const email = $('#emailContactoReg').val();
@@ -11,25 +11,26 @@ $(() => {
         const auth = new Autenticacion();
         auth.crearCuentaEmailPass(email, password, nombres);
     });
-
+    
     $("#btnInicioEmail").click(() => {
         const email = $('#emailSesion').val();
         const password = $('#passwordSesion').val();
         // TODO : LLamar auth cuenta con email
         const auth = new Autenticacion();
         auth.autEmailPass(email, password);
-
+        
     });
-
+    
     $("#authGoogle").click(() => objAuth.authCuentaGoogle() );
-
-    //$("#authTwitter").click(() => //AUTH con Twitter);
-
+    $("#authFB").click(() => objAuth.authCuentaFacebook());
+    
+    // $("#authTwitter").click(() => objAuth.authTwitter());
+    
     $('#btnRegistrarse').click(() => {
         $('#modalSesion').modal('close');
         $('#modalRegistro').modal('open');
     });
-
+    
     $('#btnIniciarSesion').click(() => {
         $('#modalRegistro').modal('close');
         $('#modalSesion').modal('open');
